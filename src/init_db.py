2911a1f9e921ad71236f7d52278cb5cd926ad96d
Zakaria.B <77070546+zakzaki244@users.noreply.github.com>
2025-06-11 14:14:24 -0400

    ajout produit magasin 2

--- src/init_db.py
+++ src/init_db.py
@@ -12,24 +12,15 @@ if session.query(Store).count() == 0:
     session.add_all([magasin1, magasin2])
     session.commit()
     print("Magasins créés.")
+else:
+    magasin1 = session.query(Store).filter_by(name="Magasin A").first()
+    magasin2 = session.query(Store).filter_by(name="Magasin B").first()
 
-# Ajoute des produits au magasin 1
-magasin1 = session.query(Store).filter_by(name="Magasin A").first()
-if magasin1 and session.query(Product).count() == 0:
+# Ajoute des produits **si la table est vide**
+if session.query(Product).count() == 0:
     produits = [
         Product(name="Bouteille d'eau", category="Boisson", price=1.5, stock=50, store=magasin1),
         Product(name="Chocolat", category="Snack", price=2.0, stock=30, store=magasin1),
-    ]
-    session.add_all(produits)
-    session.commit()
-    print("Produits ajoutés.")
-else:
-    print("Magasins/Produits déjà présents.")
-
-# Ajoute des produits au magasin 2
-magasin2 = session.query(Store).filter_by(name="Magasin B").first()
-if magasin2 and session.query(Product).count() == 0:
-    produits = [
         Product(name="Coca Cola", category="Boisson", price=1.5, stock=150, store=magasin2),
         Product(name="Sandwich", category="Snack", price=2.0, stock=400, store=magasin2),
     ]
